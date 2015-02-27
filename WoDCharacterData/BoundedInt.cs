82861f3321d0c50b84809f1c366633bae4172b77
Syndaryl <emily.smirle@gmail.com>
2015-02-27 18:13:28 -0500

    Added Bounded generic which correctly implements a bunch of interfaces. But I have some kind of weird typo in my class declaration.

--- WoDCharacterData/BoundedInt.cs
+++ WoDCharacterData/BoundedInt.cs
@@ -6,6 +6,100 @@ using System.Xml.Serialization;
 
 namespace Games.RPG.WoDCharacterData
 {
+    public class Bounded<T> : 
+        IComparable<T>, IComparable<Bounded<T>>, IEquatable<T>, IEquatable<Bounded<T>>
+        where T:IComparable<T>  {
+        #region Constructors
+        public Bounded<T>()
+        {
+        }
+
+        public Bounded(T d)
+        {
+            this.Value = d;
+        }
+        #endregion Constructors
+
+        #region Properties
+        [XmlText()]
+        public T Value {
+            get {
+                return value;
+            }
+            set {
+                if (value.CompareTo(Min) < 0)
+                    value = Min;
+                if (value.CompareTo(Max) > 0)
+                    value = Max;
+                this.value = value;
+            }
+        }
+
+        [XmlAttribute("Min")]
+        public T Min {
+            get {
+                return min;
+            }
+            set {
+                if (value.CompareTo(Max) > 0)
+                    value = Max;
+                this.min = value;
+            }
+        }
+
+        [XmlAttribute("Max")]
+        public T Max
+        {
+            get
+            {
+                return max;
+            }
+            set
+            {
+                if (value.CompareTo(Min) < 0 )
+                    value = Min;
+                this.max = value;
+            }
+        }
+        #endregion Properties
+
+        #region fields
+        private T value;
+        private T max;
+        private T min;
+        #endregion fields
+
+        #region Operators and Interfaces
+        public static implicit operator T(Bounded<T> d)  // implicit BoundedInt to int conversion operator
+        {
+            return d.Value;  // implicit conversion
+        }
+        public static implicit operator Bounded<T>(T d)  // implicit BoundedInt to int conversion operator
+        {
+            return new Bounded<T>(d);  // implicit conversion
+        }
+    
+        int IComparable<T>.CompareTo(T other)
+        {
+ 	        return Value.CompareTo(other);
+        }
+
+        int IComparable<Bounded<T>>.CompareTo(Bounded<T> other)
+        {
+ 	        return Value.CompareTo(other.Value);
+        }
+    
+        bool IEquatable<T>.Equals(T other)
+        {
+ 	        return Value.Equals(other);
+        }
+
+        bool IEquatable<Bounded<T>>.Equals(Bounded<T> other)
+        {
+ 	        return Value.Equals(other.Value);
+        }
+        #endregion
+    }
 
     public class BoundedInt
     {
