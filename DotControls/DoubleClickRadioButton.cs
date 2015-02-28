11de860c820806aa0c3e4824b2c8b3c5c6af30d7
Syndaryl <emily.smirle@gmail.com>
2015-02-28 15:47:40 -0500

    Added custom double-click radio button This is for "zeroing" out Abilities.

---
+++ DotControls/DoubleClickRadioButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Syndaryl.Windows.Forms {
+    //public partial class DoubleClickRadioButton : Control {
+    //    public DoubleClickRadioButton() {
+    //        InitializeComponent();
+    //    }
+
+    //    protected override void OnPaint(PaintEventArgs pe) {
+    //        base.OnPaint(pe);
+    //    }
+    //}
+
+    public partial class DoubleClickRadioButton : System.Windows.Forms.RadioButton {
+        public DoubleClickRadioButton() {
+            InitializeComponent();
+
+            this.SetStyle(ControlStyles.StandardClick | ControlStyles.StandardDoubleClick, true);
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Always), Browsable(true)]
+        public new event MouseEventHandler MouseDoubleClick;
+
+        protected override void OnMouseDoubleClick(MouseEventArgs e) {
+            base.OnMouseDoubleClick(e);
+
+            // raise the event
+            if (this.MouseDoubleClick != null)
+                this.MouseDoubleClick(this, e);
+        }
+    }
+}
