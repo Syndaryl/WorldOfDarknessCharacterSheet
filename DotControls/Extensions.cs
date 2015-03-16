b33e28c69c347b439bc82e257a7cf89b032ce306
Syndaryl <emily.smirle@gmail.com>
2015-03-16 10:02:19 -0400

    Fixed coordinate system error. Checkbox showing a scribble for Aggravated damage, but working!

--- DotControls/Extensions.cs
+++ DotControls/Extensions.cs
@@ -7,28 +7,28 @@ using System.Windows.Forms;
 namespace Syndaryl.Windows.Forms.Extensions {
     public static class Extensions {
         public static Point TopLeft(this Control @this) {
-            return new Point(@this.Left, @this.Top);
+            return new Point(0, 0);
         }
         public static Point TopRight(this Control @this) {
-            return new Point(@this.Right, @this.Top);
+            return new Point(@this.Width, 0);
         }
         public static Point BottomLeft(this Control @this) {
-            return new Point(@this.Left, @this.Bottom);
+            return new Point(0, @this.Height);
         }
         public static Point BottomRight(this Control @this) {
-            return new Point(@this.Right, @this.Bottom);
+            return new Point(@this.Width, @this.Height);
         }
         public static Point BottomCenter(this Control @this) {
-            return new Point((@this.Left + @this.Right) / 2, @this.Bottom);
+            return new Point((0 + @this.Width) / 2, @this.Height);
         }
         public static Point TopCenter(this Control @this) {
-            return new Point((@this.Left + @this.Right) / 2, @this.Bottom);
+            return new Point((0 + @this.Width) / 2, @this.Height);
         }
         public static Point LeftCenter(this Control @this) {
-            return new Point(@this.Left, (@this.Top + @this.Bottom) / 2);
+            return new Point(0, (0 + @this.Height) / 2);
         }
         public static Point RightCenter(this Control @this) {
-            return new Point(@this.Right, (@this.Top + @this.Bottom) / 2);
+            return new Point(@this.Width, (0 + @this.Height) / 2);
         }
 
 
